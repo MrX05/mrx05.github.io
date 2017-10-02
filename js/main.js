ea16d068ce512624674096bb930e53d0832c0849
Mr_X <amalbigeev1@gmail.com>
2017-10-02 17:01:26 +0500

    Smooth scroll

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var scroll = new SmoothScroll();
+
+var about = document.getElementById('About');
+var project = document.getElementById('Project');
+var contacts = document.getElementById('Contacts');
